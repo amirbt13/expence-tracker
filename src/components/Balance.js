47b0bf7039bfaec7ac7ede613d5facb47ad0e855
Amir Mohammad Basirati <amirbasirati13@gmail.com>
2022-08-13 18:16:03 +0430

    incomes and expences got defined

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
-const Balance = () => {
+const Balance = ({ balance }) => {
+
+  const getSum = (total, amount) => {
+    return total + +amount.amount
+  }
+
+  const totalBalance = balance.reduce(getSum, 0)
+
+  const incomes = balance.filter(transition => transition.isExpence === false)
+  const expences = balance.filter(transition => transition.isExpence === true)
+
+  const totalIncomesAmount = incomes.reduce(getSum, 0)
+  const totalExpencesAmount = expences.reduce(getSum, 0)
+
   return (
     <div>
-      <h2>Total Balance :</h2>
+      <h2>Total Balance: {totalBalance}</h2>
       <div>
-         <h4>incomes:</h4><span>$</span>
-         <h4>expence:</h4><span>$</span>
+         <h4>incomes:</h4><span>${totalIncomesAmount}</span>
+         <h4>expence:</h4><span>${totalExpencesAmount}</span>
       </div>
     </div>
   )
